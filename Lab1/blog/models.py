3bb7153a04bb467f6db768c0342e1ce2c0143a72
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2026-03-20 00:08:22 +0100

    Complete level 2 comment model and add comments to articles for Lab1

--- Lab1/blog/models.py
+++ Lab1/blog/models.py
@@ -13,7 +13,18 @@ class Article(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     text = models.TextField("Текст")
     published_at = models.DateTimeField("Дата публікації", auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='articles')
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField("Автор", max_length=100)
+    text = models.TextField("Текст")
+    commented_at = models.DateTimeField("Дата коментування", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.text[:20]}"
+    
