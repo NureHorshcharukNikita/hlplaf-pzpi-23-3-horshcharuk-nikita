f6dd8829ec199af2d32f4f0140f2bbf144675738
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2026-03-19 23:30:17 +0100

    Complete level 1 article and category models for Lab1

--- Lab1/blog/models.py
+++ Lab1/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField("Назва", max_length=100)
+    description = models.TextField("Опис", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField("Заголовок", max_length=200)
+    text = models.TextField("Текст")
+    published_at = models.DateTimeField("Дата публікації", auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
