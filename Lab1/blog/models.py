66d2b19aa35e6c46892b2b759cd2ee31304a6086
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2026-03-20 13:05:11 +0100

    Complete level 3 add pagination to articles for Lab1

--- Lab1/blog/models.py
+++ Lab1/blog/models.py
@@ -13,14 +13,14 @@ class Article(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     text = models.TextField("Текст")
     published_at = models.DateTimeField("Дата публікації", auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='articles')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='articles', verbose_name="Категорія")
 
     def __str__(self):
         return self.title
     
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments', verbose_name="Стаття")
     author = models.CharField("Автор", max_length=100)
     text = models.TextField("Текст")
     commented_at = models.DateTimeField("Дата коментування", auto_now_add=True)
--- Lab1/blog/views.py
+++ Lab1/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Article
+from django.core.paginator import Paginator
 
 # Create your views here.
+def article_list(request):
+    articles = Article.objects.all()
+    paginator = Paginator(articles, 1)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    current = page_obj.number
+    total = paginator.num_pages
+
+    start = max(current - 2, 1)
+    end = min(current + 2, total)
+
+    page_range = range(start, end + 1)
+
+    return render(request, 'article/article_list.html', {
+        'page_obj': page_obj,
+        'page_range': page_range,
+        'total_pages': total,
+    })
